92e00dee7a890b9ee521c4dd80875605f8567028
Rakshith DB <51615766+RakshithDB1@users.noreply.github.com>
2023-10-08 12:38:44 +0530

    Ask Question page completed

--- app/(root)/(home)/ask-question/page.tsx
+++ app/(root)/(home)/ask-question/page.tsx
@@ -1,5 +1,15 @@
-const askquestion = () => {
-  return <div>Questions</div>;
+import Question from "@/components/forms/Question";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
-export default askquestion;
+export default page;
